2e9a91588433c60c5eb71ff5a95465fc05ead493
Yury Zolnikov <yuzolnikov@gmail.com>
2024-11-17 12:51:52 +0300

    Adding flakes

--- app/schemas/sales_report.py
+++ app/schemas/sales_report.py
@@ -7,12 +7,12 @@ class SalesReportBase(BaseModel):
     date: str = Field(
         ...,
         max_length=Numerics.DATE_LENGTH
-        )
+    )
     report: str = Field(
         ...,
         min_length=Numerics.ANALYTICS_MIN,
         max_length=Numerics.ANALYTICS_LENGTH
-        )
+    )
 
 
 class SalesReportCreate(SalesReportBase):
